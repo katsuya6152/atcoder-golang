a16429ebcaabe34aa3e066c2d9574c4150946b08
katsuya-s <katsuya.6152@gmail.com>
2023-06-04 20:09:13 +0900

    init

---
+++ tessoku-book/tessoku_book_a.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var n int
+	fmt.Scanf("%d\n", &n)
+
+	// answer
+	fmt.Println(n * n)
+}
---
+++ tessoku-book/tessoku_book_bz.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// TODO edit this code, this code is for https://atcoder.jp/contests/practice/tasks/practice_1
+
+	// param
+	var a, b int
+	fmt.Scanf("%d %d\n", &a, &b)
+	// answer
+	fmt.Printf("%d\n", a+b)
+}
