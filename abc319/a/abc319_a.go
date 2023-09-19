cc38a12979890d66a158e8bf1d53c72453df7fb3
katsuya-s <katsuya.6152@gmail.com>
2023-09-19 09:47:04 +0900

    abc319-ab

---
+++ abc319/a/abc319_a.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s string
+	fmt.Scanf("%s\n", &s)
+
+	switch s {
+	case "tourist":
+		fmt.Println(3858)
+	case "ksun48":
+		fmt.Println(3679)
+	case "Benq":
+		fmt.Println(3658)
+	case "Um_nik":
+		fmt.Println(3648)
+	case "apiad":
+		fmt.Println(3638)
+	case "Stonefeang":
+		fmt.Println(3630)
+	case "ecnerwala":
+		fmt.Println(3613)
+	case "mnbvmar":
+		fmt.Println(3555)
+	case "newbiedmy":
+		fmt.Println(3516)
+	case "semiexp":
+		fmt.Println(3481)
+
+	}
+}
